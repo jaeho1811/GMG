4d9dfe98fb6f059b266ee420ca63f0b7d2c9e52e
GMG <241811@yangdong.sen.ms,kr>
2025-08-22 14:45:59 +0900

    GMG

--- GMG.js
+++ GMG.js
@@ -2,7 +2,7 @@ zepScript.onInit(function() {
     let _timer = 50;
     let _time = 0; // _time 변수 선언 및 초기화
 
-    zepScript.showPopup("어떤 선택지를 고르시겠습니까?", [
+    App.showCenterLabel("어떤 선택지를 고르시겠습니까?", [
         { label: "선택지 1", value: "option1" },
         { label: "선택지 2", value: "option2" },
     ], function(selected){
@@ -15,11 +15,11 @@ zepScript.onInit(function() {
         _time = _timer; // _time에 현재 값 반영
 
         if(_timer <= 0){
-            zepScript.showPopup("지구 멸망");
+            App.showCenterLabel("지구 멸망");
         } else if(_timer >= 100){
-            zepScript.showPopup("지구 환경 회복");
+            App.showCenterLabel("지구 환경 회복");
         } else {
-            zepScript.showPopup("현재 시간: " + _time);
+            App.showCenterLabel("현재 시간: " + _time);
         }
         console.log("선택된 값:", selected, "time:", _time);
     });

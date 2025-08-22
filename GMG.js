a30ae4526d12d513d9d189983d6a33e4f48016f9
GMG <241811@yangdong.sen.ms,kr>
2025-08-22 14:33:51 +0900

    GMG

--- GMG.js
+++ GMG.js
@@ -1,11 +1,10 @@
 let _timer = 50
-let _time;
 
 
 if(_timer <= 0){
 	zepScript.showPopup("지구 멸망")
 }
-if(timer >=100){
+if(_timer >=100){
     zepScript.showPopup("지구 환경 회복")
 }
 zepScript.showPopup("어떤 선택지를 고르시겠습니까?", [
@@ -16,7 +15,7 @@ zepScript.showPopup("어떤 선택지를 고르시겠습니까?", [
     if(selected === "option1") {
         _timer+=10
     } else if(selected === "option2") {
-        _time -=10
+        _timer -=10
     }
     console.log("선택된 값:", selected, "time:", _time);
 });

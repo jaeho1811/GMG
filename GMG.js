ba801dc97561028d59b9fa049d89fea74eec7521
GMG <241811@yangdong.sen.ms,kr>
2025-08-22 14:54:31 +0900

    ㅎㅎㅎㅎ

--- GMG.js
+++ GMG.js
@@ -1,27 +1,29 @@
 zepScript.onInit(function() {
     let _timer = 50;
-    let _time = 0;
 
     function showChoice() {
         zepScript.showPopup("어떤 선택지를 고르시겠습니까?", [
             { label: "선택지 1", value: "option1" },
-            { label: "선택지 2", value: "option2" },
+            { label: "선택지 2", value: "option2" }
         ], function(selected){
             if(selected === "option1") {
                 _timer += 10;
             } else if(selected === "option2") {
                 _timer -= 10;
             }
-            _time = _timer;
 
             if(_timer <= 0){
                 zepScript.showPopup("지구 멸망");
             } else if(_timer >= 100){
                 zepScript.showPopup("지구 환경 회복");
             } else {
-                zepScript.showPopup("현재 시간: " + _time, [], showChoice);
+                zepScript.showPopup("현재 시간: " + _timer, [
+                    { label: "다음 선택", value: "next" }
+                ], function() {
+                    showChoice();
+                });
             }
-            console.log("선택된 값:", selected, "time:", _time);
+            console.log("선택된 값:", selected, "timer:", _timer);
         });
     }
 

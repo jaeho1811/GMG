a433342648117e5518b376d036409cb1432560cc
jaeho1811 <241811@yangdong.sen.ms.kr>
2025-09-12 13:55:08 +0900

    ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ

--- GMG.js
+++ GMG.js
@@ -1,10 +1,10 @@
-// zepScript에서 실행되는 코드로 변환
+// zep에서 실행되는 코드로 변환
 
 let _timer = 50;
 const 정답 = "option1"; // 정답을 원하는 값으로 설정
 
 function showChoice() {
-    zepScript.showPopup("어떤 선택지를 고르시겠습니까?", [
+    zep.showPopup("어떤 선택지를 고르시겠습니까?", [
         { label: "선택지 1", value: "option1" },
         { label: "선택지 2", value: "option2" }
     ], function(selected){
@@ -15,11 +15,11 @@ function showChoice() {
         }
 
         if(_timer <= 0){
-            zepScript.showPopup("지구 멸망! 게임 종료");
+            zep.showPopup("지구 멸망! 게임 종료");
         } else if(_timer >= 100){
-            zepScript.showPopup("지구 환경 회복! 게임 종료");
+            zep.showPopup("지구 환경 회복! 게임 종료");
         } else {
-            zepScript.showPopup("현재 시간: " + _timer, [
+            zep.showPopup("현재 시간: " + _timer, [
                 { label: "다음 선택", value: "next" }
             ], function() {
                 showChoice();
@@ -29,6 +29,6 @@ function showChoice() {
     });
 }
 
-zepScript.onInit(function() {
+zep.onInit(function() {
     showChoice();
 });
